d963dcf0213c05a18d0174092d25262ca3dfe3ec
warP1000 <55244080+warP1000@users.noreply.github.com>
2019-09-13 10:54:01 +0800

    Add files via upload

--- 小玉家的电费.cpp
+++ 小玉家的电费.cpp
@@ -2,12 +2,14 @@
 using namespace std;
 int main()
 {
-	int begin,over;
+	int begin;
 		cin>>begin;
-	if(begin<=400)
+	if(begin<=150)
 		cout<<begin*0.4463;
+	if( begin>150,begin<=400)
+		cout<<150*0.4463+(begin-150)*0.4663;
 	if(begin>400)
-		cout<<400*0.4463+(begin-400)*0.5663;
+		cout<<150*0.4463+(400-150)*0.4663+(begin-400)*0.5663;
 
 		
 	return 0;
